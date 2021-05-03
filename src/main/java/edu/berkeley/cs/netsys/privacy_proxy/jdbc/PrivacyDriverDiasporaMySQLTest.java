5883e54943ebb7569e0f4c550b14843fab12bc00
Wen Zhang <zhangwen@cs.berkeley.edu>
2021-05-03 09:59:47 -0700

    test from laptop

--- src/main/java/edu/berkeley/cs/netsys/privacy_proxy/jdbc/PrivacyDriverDiasporaMySQLTest.java
+++ src/main/java/edu/berkeley/cs/netsys/privacy_proxy/jdbc/PrivacyDriverDiasporaMySQLTest.java
@@ -9,12 +9,12 @@ import java.sql.*;
 
 public class PrivacyDriverDiasporaMySQLTest {
     private static final String dbDatabaseName = "diaspora_production";
-    private static final String dbUrl = "jdbc:mysql://localhost:3306/" + dbDatabaseName +
+    private static final String dbUrl = "jdbc:mysql://diaspora.internal:3306/" + dbDatabaseName +
             "?useSSL=false&useUnicode=true&character_set_server=utf8mb4&collation_server=utf8mb4_bin";
     private static final String dbUsername = "diaspora";
     private static final String dbPassword = "12345678";
 
-    private static final String setupDbDir = "/home/ubuntu/setup_db";
+    private static final String setupDbDir = "/Users/zhangwen/scratch/setup_db";
     private static final String setupDbPath = setupDbDir + "/db";
     private static final String setupDbUrl = "jdbc:h2:" + setupDbPath;
     // I think the setup DB is required to have the same username / password as the actual DB.
@@ -49,12 +49,12 @@ public class PrivacyDriverDiasporaMySQLTest {
         Class.forName("com.mysql.jdbc.Driver");
 
         String proxyUrl = String.format("jdbc:privacy:thin:%s,%s,%s,%s,%s,%s,%s",
-                "/home/ubuntu/diaspora/policy/policies.sql", // Policy file.
-                "/home/ubuntu/diaspora/policy/deps.txt", // Misc dependencies.
+                "/Users/zhangwen/code/diaspora/policy/policies.sql", // Policy file.
+                "/Users/zhangwen/code/diaspora/policy/deps.txt", // Misc dependencies.
                 setupDbUrl,
                 dbUrl,
-                "/home/ubuntu/diaspora/policy/pk.txt", // Primary key dependencies.
-                "/home/ubuntu/diaspora/policy/fk.txt", // Foreign key dependencies.
+                "/Users/zhangwen/code/diaspora/policy/pk.txt", // Primary key dependencies.
+                "/Users/zhangwen/code/diaspora/policy/fk.txt", // Foreign key dependencies.
                 dbDatabaseName
         );
 
@@ -62,22 +62,22 @@ public class PrivacyDriverDiasporaMySQLTest {
 
         try (PrivacyConnection conn = (PrivacyConnection) DriverManager.getConnection(proxyUrl, dbUsername, dbPassword)) {
             for (int i = 0; i < 1; i++) {
-                try (Statement stmt = conn.createStatement()) {
-                    stmt.execute("SET @_MY_UID = 2");
-                }
+//                try (Statement stmt = conn.createStatement()) {
+//                    stmt.execute("SET @_MY_UID = 2");
+//                }
 
-                {
-                    final String query1 = "SELECT  `users`.* FROM `users` WHERE `users`.`id` = ? ORDER BY `users`.`id` ASC LIMIT ?";
-                    try (PreparedStatement stmt = conn.prepareStatement(query1)) {
-                        stmt.setInt(1, 2);
-                        stmt.setInt(2, 1);
-                        stmt.execute();
-                        try (ResultSet rs = stmt.getResultSet()) {
-                            while (rs.next()) {
-                            }
-                        }
-                    }
-                }
+//                {
+//                    final String query1 = "SELECT  `users`.* FROM `users` WHERE `users`.`id` = ?_MY_UID ORDER BY `users`.`id` ASC LIMIT ?";
+//                    try (PreparedStatement stmt = conn.prepareStatement(query1)) {
+//                        stmt.setInt(1, 2);
+//                        stmt.setInt(2, 1);
+//                        stmt.execute();
+//                        try (ResultSet rs = stmt.getResultSet()) {
+//                            while (rs.next()) {
+//                            }
+//                        }
+//                    }
+//                }
 
 //                {
 //                    final String query2 = "SELECT  posts.* FROM `posts` INNER JOIN `share_visibilities` ON `share_visibilities`.`shareable_id` = `posts`.`id` AND `share_visibilities`.`shareable_type` = ? WHERE `posts`.`id` = ? AND `share_visibilities`.`user_id` = ?";
@@ -94,7 +94,7 @@ public class PrivacyDriverDiasporaMySQLTest {
 //                }
 
                 {
-                    final String query3 = "SELECT  `people`.* FROM `people` WHERE `people`.`owner_id` = ? LIMIT ?";
+                    final String query3 = "SELECT  `people`.* FROM `people` WHERE `people`.`owner_id` = ?_MY_UID LIMIT ?";
                     try (PreparedStatement stmt = conn.prepareStatement(query3)) {
                         stmt.setInt(1, 2);
                         stmt.setInt(2, 1);
@@ -107,14 +107,14 @@ public class PrivacyDriverDiasporaMySQLTest {
                 }
 
                 {
-//                    final String query4 = "SELECT  `posts`.* FROM `posts` WHERE `posts`.`id` = ? AND `posts`.`author_id` = ? ORDER BY `posts`.`id` ASC LIMIT ?";
-                    final String query4 = "(SELECT  `posts`.* FROM `posts` WHERE `posts`.`id` = ? AND `posts`.`author_id` = ?) UNION (SELECT  posts.* FROM `posts` INNER JOIN `share_visibilities` ON `share_visibilities`.`shareable_id` = `posts`.`id` AND `share_visibilities`.`shareable_type` = ? WHERE `posts`.`id` = ? AND `share_visibilities`.`user_id` = ?)";
+//                    final String query4 = "(SELECT  `posts`.* FROM `posts` WHERE `posts`.`id` = ? AND `posts`.`author_id` = ?) UNION (SELECT  posts.* FROM `posts` INNER JOIN `share_visibilities` ON `share_visibilities`.`shareable_id` = `posts`.`id` AND `share_visibilities`.`shareable_type` = ? WHERE `posts`.`id` = ? AND `share_visibilities`.`user_id` = ?)";
+                    final String query4 = "SELECT  `posts`.* FROM `posts` WHERE `posts`.`id` = ? AND `posts`.`author_id` = ?";
                     try (PreparedStatement stmt = conn.prepareStatement(query4)) {
                         stmt.setInt(1, 4);
                         stmt.setInt(2, 3);
-                        stmt.setString(3, "Post");
-                        stmt.setInt(4, 4);
-                        stmt.setInt(5, 2);
+//                        stmt.setString(3, "Post");
+//                        stmt.setInt(4, 4);
+//                        stmt.setInt(5, 2);
                         stmt.execute();
                         try (ResultSet rs = stmt.getResultSet()) {
                             while (rs.next()) {
@@ -124,25 +124,26 @@ public class PrivacyDriverDiasporaMySQLTest {
                 }
 
                 {
-                    final String query5 = "SELECT  profiles.id, profiles.diaspora_handle, profiles.first_name, profiles.last_name, profiles.image_url, profiles.image_url_small, profiles.image_url_medium, profiles.searchable, profiles.person_id, profiles.created_at, profiles.updated_at, profiles.full_name, profiles.nsfw, profiles.public_details FROM `profiles` WHERE `profiles`.`person_id` = ? LIMIT ?";
+                    final String query5 = "SELECT  `notifications`.* FROM `notifications` WHERE `notifications`.`recipient_id` = ? ORDER BY updated_at desc LIMIT ?";
                     try (PreparedStatement stmt = conn.prepareStatement(query5)) {
-                        stmt.setInt(1, 3);
-                        stmt.setInt(2, 1);
+                        stmt.setInt(1, 2);
+                        stmt.setInt(2, 10);
                         stmt.execute();
                         try (ResultSet rs = stmt.getResultSet()) {
                             while (rs.next()) {
+                                System.out.println("\t" + rs.getString(2) + ", " + rs.getInt(3));
                             }
                         }
                     }
                 }
 
                 {
-//                    final String query6 = "SELECT COUNT(*) FROM (SELECT DISTINCT photos.* FROM `photos` LEFT OUTER JOIN share_visibilities ON share_visibilities.shareable_id = photos.id AND share_visibilities.shareable_type = 'Photo' WHERE `photos`.`author_id` = ? AND (`share_visibilities`.`user_id` = 2 OR `photos`.`public` = 1) AND (photos.created_at < '2021-04-24 21:10:20.493711') AND `photos`.`pending` = ? ORDER BY photos.created_at DESC, created_at DESC) subquery_for_count";
-                    final String query6 = "SELECT  `conversation_visibilities`.`id` AS t0_r0, `conversation_visibilities`.`conversation_id` AS t0_r1, `conversation_visibilities`.`person_id` AS t0_r2, `conversation_visibilities`.`unread` AS t0_r3, `conversation_visibilities`.`created_at` AS t0_r4, `conversation_visibilities`.`updated_at` AS t0_r5, `conversations`.`id` AS t1_r0, `conversations`.`subject` AS t1_r1, `conversations`.`guid` AS t1_r2, `conversations`.`author_id` AS t1_r3, `conversations`.`created_at` AS t1_r4, `conversations`.`updated_at` AS t1_r5 FROM `conversation_visibilities` LEFT OUTER JOIN `conversations` ON `conversations`.`id` = `conversation_visibilities`.`conversation_id` WHERE `conversation_visibilities`.`person_id` = ? ORDER BY conversations.updated_at DESC LIMIT ? OFFSET ?";
+//                    final String query6 = "SELECT `posts`.* FROM `posts` WHERE `posts`.`id` IN (?, ?, ?)";
+                    final String query6 = "SELECT `posts`.* FROM `posts` WHERE `posts`.`id` = 42";
                     try (PreparedStatement stmt = conn.prepareStatement(query6)) {
-                        stmt.setInt(1, 3);
-                        stmt.setInt(2, 15);
-                        stmt.setInt(3, 0);
+//                        stmt.setInt(1, 42);
+//                        stmt.setInt(2, 10);
+//                        stmt.setInt(3, 2);
                         stmt.execute();
                         try (ResultSet rs = stmt.getResultSet()) {
                             while (rs.next()) {
