af1027d33ef9d6397238f7a3934c75898937e7e9
Wen Zhang <zhangwen@cs.berkeley.edu>
2021-06-28 23:20:12 -0700

    imported policy boolean fix

--- src/test/resources/DiasporaTest/deps.sql
+++ src/test/resources/DiasporaTest/deps.sql
@@ -5,5 +5,7 @@
 SELECT users.id FROM users; SELECT owner_id FROM people;
 
 -- reshare_constraints: (assert (forall (posts tuple t) (=> (and (t in posts) (= t.type C_RESHARE)) (and (= t.public 1) (exists (posts tuple tr) (and (tr in posts) (= t.root_guid tr.guid) (= tr.public 1)))))))
-SELECT posts.guid FROM posts WHERE posts.type = 'Reshare' AND posts.public = 0; SELECT posts.guid FROM posts WHERE FALSE;
+-- In our current implementation, we must write `posts.public <> 1` instead of `posts.public = 0` because we model
+-- a Boolean field as an integer and don't constrain its value to be {0, 1}.
+SELECT posts.guid FROM posts WHERE posts.type = 'Reshare' AND posts.public <> 1; SELECT posts.guid FROM posts WHERE FALSE;
 SELECT posts.root_guid FROM posts WHERE posts.type = 'Reshare'; SELECT posts.guid FROM posts WHERE posts.public = 1;
